d5ce15bc2e3bd16ac1edb190fe65cc24210d8a3b
SN <60329264+Neo-XXX@users.noreply.github.com>
2025-07-19 10:55:28 -0500

    chore: load dotenv in db helper

--- models/Mcc.js
+++ models/Mcc.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 
 const MccSchema = new mongoose.Schema({
-  // Some records may store the code as a number, others as a string
   mcc_code: mongoose.Schema.Types.Mixed,
-  category: String
+  category: String,
 });
 
 module.exports = mongoose.model('Mcc', MccSchema, 'stripe_list');
-
